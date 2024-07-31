1f37ee6fd8aa5218bbc25f924a731b31bf8d7d5d
carmen-ruiz <carmenines2000@gmail.com>
2024-07-31 12:06:07 +0200

    Delete unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
 function toggleMenu() {
     document.getElementById("nav").classList.toggle("hidden");    
   }
-
- function checkToggleMenu(event) {
-  if (event.code === 'Enter') toggleMenu();
- }
